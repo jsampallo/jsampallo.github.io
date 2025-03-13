a3b4e94311f0ff6d8afd42afcda652fc5f2bc0e9
Joaquin Sampallo <jsampallo97@gmail.com>
2025-03-12 22:47:56 -0300

    idiomas funciona

--- script.js
+++ script.js
@@ -26,11 +26,17 @@ document.querySelectorAll("li").forEach(n => n.addEventListener("click", ()=> {
 // IDIOMAS
 let language = navigator.language;
 let lang = language.substr(0, 2);
-var menuhome = document.getElementById("menuhome")
-var menuabout = document.getElementById("menuabout")
-var menuwork = document.getElementById("menuwork")
-var menucontact = document.getElementById("menucontact")
-var desc = document.getElementById("desc")
+var menuhome = document.getElementById("menuhome");
+var menuabout = document.getElementById("menuabout");
+var menuwork = document.getElementById("menuwork");
+var menucontact = document.getElementById("menucontact");
+var desc = document.getElementById("desc");
+var knowmorebtn = document.getElementById("knowmorebtn");
+var h2a = document.getElementById("h2a");
+var h2b = document.getElementById("h2b");
+var you = document.getElementById("you");
+var questiontext = document.getElementById("questiontext");
+var sendemail = document.getElementById("sendemail");
 
 
 
@@ -39,5 +45,11 @@ if (lang === 'es') {
     menuabout.innerHTML = "Sobre Mí";
     menuwork.innerHTML = "Trabajos";
     menucontact.innerHTML = "Contacto";
-    desc.innerHTML = "Soy diseñador visual<br>de productos digitales";
+    desc.innerHTML = "Diseñador visual<br>de productos digitales";
+    knowmorebtn.innerHTML = "SABER MÁS";
+    h2a.innerHTML = "¿Qué ofrezco?";
+    h2b.innerHTML = "Trabajos recientes";
+    you.innerHTML = "Ahora, me gustaría<br>que me cuentes";
+    questiontext.innerHTML = "¿Cómo se ve tu proyecto?";
+    sendemail.innerHTML = "ESCRIBIR E-MAIL"
 }else{}
